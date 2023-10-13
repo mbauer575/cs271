8076e0cf5c4bea9e0e52b01b2489d25434600db0
mbauer575 <bauermax@oregonstate.edu>
2023-10-13 11:33:43 -0700

    co3 maybe

--- cplorations/c03/main.c
+++ cplorations/c03/main.c
@@ -1,12 +1,55 @@
 /****************************************
  * C-ploration 3 for CS 271
  * 
- * [NAME] $YOUR_NAME$
- * [TERM] FALL $YEAR$
+ * [NAME] Maxwell Bauer
+ * [TERM] FALL 2023
  * 
  ****************************************/
- 
+ #include <stdlib.h>
+ #include <stdio.h>
+ #include <string.h>
+
+#define MAX_LINE_LENGTH  200
+
+
 int main(int argc, const char *argv[])
 {	
+	if(argc != 2)
+	{
+		printf("Usage: %s [filename]\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}	
+
+	char input_file[MAX_LINE_LENGTH];
+	strcpy(input_file, "asm/");
+	strcat(input_file, argv[1]);
+
+	FILE *fin = fopen(input_file, "r");
+
+	if(fin == NULL)
+	{
+		perror("Unable to open file!");
+		exit(EXIT_FAILURE);
+	}
+	
+	char out_file[MAX_LINE_LENGTH];
+	
+	strcpy(out_file, argv[1]);
+	strcat(out_file, ".echo");
+
+	FILE *fout = fopen(out_file, "w+");
+
+
+	char line[MAX_LINE_LENGTH] = {0};
+	unsigned int line_num = 0;
+
+	while (fgets(line, sizeof(line), fin)) 
+	{
+		line_num++;
+    	printf("[%04d] %s", line_num, line);
+    	fprintf(fout, "[%04d] %s", line_num, line);
+	}
+	fclose(fin);
+	fclose(fout);
 	return 1;
 }
