735256569706b4b5025d851235112025349e0fe9
mbauer575 <bauermax@oregonstate.edu>
2023-11-20 14:46:44 -0800

    04

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -10,3 +10,41 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+@2	
+M=0	
+
+@0
+D=M
+@END
+D;JEQ	
+
+@1
+D=M
+@END
+D;JEQ	
+
+@0
+D=M	
+@3	
+M=D	
+
+
+(LOOP)
+@1	
+D=M	
+
+@2	
+M=D+M	
+
+@3	
+M=M-1	
+
+D=M	
+@LOOP	
+D;JGT	
+
+
+(END)
+@END
+0;JMP	//loop
